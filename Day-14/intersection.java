f04f20a6b0c0a2bcf5e511df3f2824c4e338135b
ABIRAMI-008 <abiramiparamasivan08@gmail.com>
2026-07-24 12:32:01 +0530

    Day14 solution

---
+++ Day-14/intersection.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        Set<Integer> set1 = new HashSet<>();
+
+        // Store nums1 values
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        Set<Integer> result = new HashSet<>();
+
+        // Check nums2 values
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                result.add(num);
+            }
+        }
+
+        // Convert Set to array
+        int[] ans = new int[result.size()];
+
+        int i = 0;
+        for (int num : result) {
+            ans[i++] = num;
+        }
+
+        return ans;
+    }
+}
