41ff74fc492d66a5bbbd82c71ad38a11de771a0e
ABIRAMI-008 <abiramiparamasivan08@gmail.com>
2026-07-25 10:32:16 +0530

    Day-15 solutions

---
+++ Day-15/Readme.md
@@ -0,0 +1,17 @@
+Day 15
+
+Problem 1
+Name: Valid Palindrome
+Platform: LeetCode
+Difficulty: Easy
+Approach: Two Pointers
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Problem 2
+Name: Convert a Number to Hexadecimal
+Platform: LeetCode
+Difficulty: Easy
+Approach: Bit Manipulation
+Time Complexity: O(1)
+Space Complexity: O(1)
---
+++ Day-15/valid.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase();
+        String str = "";
+
+        // Keep only letters and digits
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                str += s.charAt(i);
+            }
+        }
+
+        // Check palindrome
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
