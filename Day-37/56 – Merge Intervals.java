7f92a182d2371b6d367b39bd2bdbbcd5ba11fc72
ABIRAMI-008 <abiramiparamasivan08@gmail.com>
2026-08-15 21:49:50 +0530

    Day-37 solved

---
+++ Day-37/56 – Merge Intervals.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> result = new ArrayList<>();
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            int currentStart = intervals[i][0];
+            int currentEnd = intervals[i][1];
+
+            if (currentStart <= end) {
+                // Overlapping
+                end = Math.max(end, currentEnd);
+            } 
+            else {
+                // Not overlapping
+                result.add(new int[]{start, end});
+
+                start = currentStart;
+                end = currentEnd;
+            }
+        }
+
+        // Add the last interval
+        result.add(new int[]{start, end});
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
