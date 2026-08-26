854ec222ef4817d704a52cee76dc410f12de7ef8
ABIRAMI-008 <abiramiparamasivan08@gmail.com>
2026-08-26 18:26:54 +0530

    Day-45 solved

---
+++ Day-45/401 – Binary Watch.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> result = new ArrayList<>();
+
+        for (int hour = 0; hour < 12; hour++) {
+            for (int minute = 0; minute < 60; minute++) {
+
+                int count = Integer.bitCount(hour) + Integer.bitCount(minute);
+
+                if (count == turnedOn) {
+                    result.add(String.format("%d:%02d", hour, minute));
+                }
+            }
+        }
+
+        return result;
+    }
+}
