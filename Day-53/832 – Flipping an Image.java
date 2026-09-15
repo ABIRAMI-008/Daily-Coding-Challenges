3bc8968819d3e307b1e62d3fdd10985d254e6fcb
ABIRAMI-008 <abiramiparamasivan08@gmail.com>
2026-09-15 10:09:12 +0530

    solved

---
+++ Day-53/832 – Flipping an Image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        
+        for (int[] row : image) {
+            int left = 0;
+            int right = row.length - 1;
+
+            while (left <= right) {
+                // Reverse + invert at the same time
+                int temp = row[left];
+                row[left] = 1 - row[right];
+                row[right] = 1 - temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        return image;
+    }
+}
